5de7aadb0ea2d89349956492a8780898d31781c7
GAI-313 <nakatogawagai@gmail.com>
2025-05-28 09:20:46 +0900

    first commit

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+import os
+
+project = 'tello_console3'
+copyright = '2025, Gai Nakatogawa'
+author = 'Gai Nakatogawa'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',       # docstring からドキュメントを生成
+    'sphinx.ext.napoleon',      # Google/NumPy スタイルの docstring を解釈
+    'sphinx.ext.viewcode',      # ソースコードへのリンクを追加
+    'sphinx_autodoc_typehints', # 型ヒントを表示
+    'sphinx_rtd_theme',         # Read the Docs テーマ
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+autodoc_member_order = 'bysource'
